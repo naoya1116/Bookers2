51a15971cdf4d45a89690ceaf33ae5d144f33e37
naoya1116 <notice.jr@gmail.com>
2021-04-01 13:56:32 +0000

    [remake]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,35 @@
 class BooksController < ApplicationController
   
-  def new
-  end
-
-  def create
-  end
-
   def index
+    @book = Book.new
+    @books = Book.all
   end
+  
+   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+   end
 
   def show
+  # @book = Book.new  
+   @book = Book.find(params[:id])
+  end
+  
+  def edit
+  end
+  
+  def update
   end
 
   def destroy
   end
+  
+  private
+
+  def book_params
+   params.require(:book).permit(:title, :body)
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,26 @@
 class UsersController < ApplicationController
+  
   def index
+    @users = User.all
   end
-    
+  
   def show
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to book_path(@book.id)
+  end 
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
   end
   
 end
