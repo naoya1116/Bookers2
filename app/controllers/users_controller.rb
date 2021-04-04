7bb5d1384f746e3a5574b17181f7b73fc737c623
naoya1116 <notice.jr@gmail.com>
2021-04-04 21:14:48 +0000

    [remake]4-10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,38 @@
 class UsersController < ApplicationController
-  
+
   def index
+    @book = Book.new
     @users = User.all
   end
-  
-  def show
+
+  def create
+    # @book = Book.new(book_params)
+    # @book.user_id = current_user.id
+    # @book.save
+    # flash[:notice] = “Welcome! You have signed up successfully.”
+    # redirect_to book_path(@book.id)
   end
-  
+
+  def show
+   @book = Book.new
+   @user = User.find(params[:id])
+   @books = @user.books
+  end
+
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to book_path(@book.id)
-  end 
-  
+    redirect_to user_path(@user.id)
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction)
+    params.require(:user).permit(:name, :introduction, :profile_image_id)
   end
-  
+
 end
