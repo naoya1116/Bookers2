b2b0d31a2c3a345db4a1d644881681e3f92a71fc
naoya1116 <notice.jr@gmail.com>
2021-04-08 13:16:29 +0000

    [finish]4-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '6f183c1ad60957b373d582e6a2b679c1b74ae10436aeff8a6fd552c6871af4d1e5a2a18a110e09d56c07ac3212f1e79c14f577c298039ac7f4c99853f821d2e1'
